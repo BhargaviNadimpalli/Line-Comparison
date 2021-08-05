35d2d5252eb219a48ca5d2d0d171deb0350ff467
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-04 18:11:19 -0600

    calculating the length

--- Line-Comparison/Line-Comparison/Program.cs
+++ Line-Comparison/Line-Comparison/Program.cs
@@ -6,7 +6,15 @@ namespace Line_Comparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to line comparison programs");
+            double x1, x2, y1, y2,length;
+            Console.WriteLine("Enter the values of x1,x2,y1,y2");
+            x1 = double.Parse(Console.ReadLine());
+            x2 = double.Parse(Console.ReadLine());
+            y1 = double.Parse(Console.ReadLine());
+            y2 = double.Parse(Console.ReadLine());
+            length = Math.Sqrt(Math.Pow(x2 - x1 , 2) + Math.Pow(y2- y1 , 2));
+            Console.WriteLine("The length of co-ordinates :" + length);
         }
+
     }
 }
