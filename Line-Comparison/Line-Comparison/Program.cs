f74940aaecf951e8d97eb2203eba04222e62c51c
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-04 18:31:44 -0600

    Checking the two lengths are equal or not

--- Line-Comparison/Line-Comparison/Program.cs
+++ Line-Comparison/Line-Comparison/Program.cs
@@ -6,14 +6,29 @@ namespace Line_Comparison
     {
         static void Main(string[] args)
         {
-            double x1, x2, y1, y2,length;
+            double x1, x2,x3,x4,y1, y2,y3,y4,length1,length2;
             Console.WriteLine("Enter the values of x1,x2,y1,y2");
             x1 = double.Parse(Console.ReadLine());
             x2 = double.Parse(Console.ReadLine());
             y1 = double.Parse(Console.ReadLine());
             y2 = double.Parse(Console.ReadLine());
-            length = Math.Sqrt(Math.Pow(x2 - x1 , 2) + Math.Pow(y2- y1 , 2));
-            Console.WriteLine("The length of co-ordinates :" + length);
+            Console.WriteLine("Enter the values of x3,x3,y3,y4");
+            x3 = double.Parse(Console.ReadLine());
+            x4 = double.Parse(Console.ReadLine());
+            y3 = double.Parse(Console.ReadLine());
+            y4 = double.Parse(Console.ReadLine());
+            length1 = Math.Sqrt(Math.Pow(x2 - x1 , 2) + Math.Pow(y2- y1 , 2));
+            length2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+            Console.WriteLine("The length of first co-ordinates :" + length1);
+            Console.WriteLine("The length of second co-ordinates :" + length2);
+            if (length1 == length2)
+            {
+                Console.WriteLine("The length1 and length2 are equal");
+            }
+            else
+            {
+                Console.WriteLine("The length1 and length2 are not equal");
+            }
         }
 
     }
